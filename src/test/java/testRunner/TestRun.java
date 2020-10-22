c2c07f5ea345688d55820a0aede58a714e4cb93a
hasibrahat10 <hasibrahat10@gmail.com>
2020-10-22 13:13:42 +0600

    Update workspace changes

--- src/test/java/testRunner/TestRun.java
+++ src/test/java/testRunner/TestRun.java
@@ -7,15 +7,13 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-<<<<<<< HEAD
+
       features = ".//Features/Login.feature",
         //features = {".//Features/Login.feature",".//Features/Customer.feature"},
-=======
 
-       features = ".//Features/Customer.feature",
-        //features = {".//Features/Login.feature",".//Features/Customer.feature"},
+//       features = ".//Features/Customer.feature",
+//        //features = {".//Features/Login.feature",".//Features/Customer.feature"},
 
->>>>>>> 37079e81bc4ea690a8218d5750678d24dfbe9655
         glue = "stepDefinitions",
         monochrome = true,
         plugin = {"pretty", "html:test-output"}
